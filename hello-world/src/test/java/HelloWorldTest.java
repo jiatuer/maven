4e6bd27611357eba069511708e89dcdf6b7ee477
jiazhong <jiazhong@ebay.com>
2013-11-04 23:37:16 +0800

    .

--- hello-world/src/test/java/HelloWorldTest.java
+++ hello-world/src/test/java/HelloWorldTest.java
@@ -1,7 +1,9 @@
-package com.jason.mvnbook.helloworld;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import com.jason.mvnbook.helloworld.HelloWorld;
 
 public class HelloWorldTest{
 		
